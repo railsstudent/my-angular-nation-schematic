6efaefa749b06666aa4163f02d5a2769b0db7a70
Connie Leung <cleung334@gmail.com>
2020-05-05 01:29:08 +0800

    Intro to Schematic course
    Signed-off-by: Connie Leung <cleung334@gmail.com>

---
+++ create-structure/index.ts
@@ -0,0 +1,26 @@
+import { Rule, SchematicContext, Tree, apply, url, chain, template, mergeWith, move, branchAndMerge, SchematicsException,  } 
+from '@angular-devkit/schematics';
+import { strings } from '@angular-devkit/core';
+
+// You don't have to export the function as default. You can also have more than one rule factory
+// per file.
+export function createStructure(_options: any): Rule {
+  return (tree: Tree, _context: SchematicContext) => {
+    _context.logger.info(JSON.stringify(_options));
+    
+    const angularConfig = tree.read('/angular.json');
+    if (!angularConfig) {
+      throw new SchematicsException('You are not in an Angular project, goodbye!');
+    }
+
+    const source = apply(url('./files'), [
+      template({
+        ...strings,
+        ..._options as object 
+      } as any),
+      move(_options.path)
+    ]);
+
+    return chain([branchAndMerge(chain([mergeWith(source)]))])(tree, _context);
+  };
+}
